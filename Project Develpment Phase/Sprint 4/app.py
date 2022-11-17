63b35bdee81322cbcc2ad70b390ccb10d1e27c7b
gurunivash2001 <100774725+gurunivash2001@users.noreply.github.com>
2022-11-17 19:49:31 +0530

    sprint

--- Project Develpment Phase/Sprint 4/app.py
+++ Project Develpment Phase/Sprint 4/app.py
@@ -1,63 +1,69 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 18 13:25:58 2022
-
-@author: gurun
-"""
-from flask import Flask, render_template, request
+# Import Libraries
 import pandas as pd
 import numpy as np
+from flask import Flask, render_template, Response, request
 import pickle
 from sklearn.preprocessing import LabelEncoder
-app = Flask('_name_',template_folder='template')
-filename ='resale_model.sav' 
-model_rand = pickle.load(open('price prediction.pkl','rb'))
+
+app = Flask(__name__)#initiate flask app
+
+def load_model(file='model.sav'):#load the saved model
+	return pickle.load(open(file, 'rb'))
+
 @app.route('/')
-def index():
-    return render_template('index.html')
-@app.route('/predict')
+def index():#main page
+	return render_template('car.html')
+
+@app.route('/predict_page')
+def predict_page():#predicting page
+	return render_template('value.html')
+
+@app.route('/predict', methods=['GET','POST'])
 def predict():
-    return render_template('index.html')
-@app.route('/y_predict', methods=['GET', 'POST'])
-def y_predict():
-    Selling_Price= int(request.form[ 'Selling_Price'])
-    Present_Price = float(request.form['Present_Price']) 
-    Kms_Driven = float(request.form['Kms_Driven'])
-    Years_old = int(request.fors.get('Years_old')) 
-    Transmission = request.form['Transmission']
-    Owner = request.form.get("Owner") 
-    Fuel_Type = request.form.get('Fuel_Type')
-    Seller_Type = request.form.get('Seller_Type')
-    new_row = {'yearOfRegistration':Selling_Price, 'powerPS':Present_Price, 'kilometer': Kms_Driven,
-       'monthofRegistration':Years_old, 'gearbox':Transmission,
-        'brand':Owner, 'fuel Type':Fuel_Type, 
-       'vehicleType':Seller_Type}
-    print(new_row)
-    new_df = pd.DataFrame(columns = ['vehicleType', 'yearOfRegistration', 'gearbox'
-                            'powerPS', 'kilometer', 'monthofRegistration", "fuelType',
-                            'brand' ])
-    new_df = new_df.append(new_row,ignore_index = True)
-    labels = ['gearbox','brand', 'fuel Type', 'vehicleType']
-    mapper = ()
-    for i in labels:
-        mapper[i] = LabelEncoder()
-        mapper[i].classes_ = np.load(str('classes'+i+'.npy'))
-        tr = mapper[i].fit_transform(new_df[i])
-        new_df.loc[:,i + '_ Labels'] = pd.Series (tr, index-new_df. Index)
-        labeled = new_df[ ['yearOfRegistration'
-                           ,'powerPS'
-                           ,'kilometer' 
-                           ,'monthofRegistration'
-                           ]
-                         + [x+'_Labels' for x in labels]]
-        X = labeled.values
-        print(X)
-        y_prediction = model_rand.predict(X)
-        print(y_prediction)
-        return render_template('index.html',ypred = 'The resale value predicted is (:.2f)s'.format(y_prediction[0]))
-    if __name__ == '_main_':
-        app.debug = True
-        app.run(host = '0.0.0.0', port = 5000)
-        
-        
-        
+	reg_year = int(request.args.get('regyear'))
+	powerps = float(request.args.get('powerps'))
+	kms= float(request.args.get('kms'))
+	reg_month = int(request.args.get('regmonth'))
+
+	gearbox = request.args.get('geartype')
+	damage = request.args.get('damage')
+	model = request.args.get('model')
+	brand = request.args.get('brand')
+	fuel_type = request.args.get('fuelType')
+	veh_type = request.args.get('vehicletype')
+
+	new_row = {'yearOfReg':reg_year, 'powerPS':powerps, 'kilometer':kms,
+				'monthOfRegistration':reg_month, 'gearbox':gearbox,
+				'notRepairedDamage':damage,
+				'model':model, 'brand':brand, 'fuelType':fuel_type,
+				'vehicletype':veh_type}
+
+	print(new_row)
+
+	new_df = pd.DataFrame(columns=['vehicletype','yearOfReg','gearbox',
+		'powerPS','model','kilometer','monthOfRegistration','fuelType',
+		'brand','notRepairedDamage'])
+	new_df = new_df.append(new_row, ignore_index=True)
+	labels = ['gearbox','notRepairedDamage','model','brand','fuelType','vehicletype']
+	mapper = {}
+
+	for i in labels:
+		mapper[i] = LabelEncoder()
+		mapper[i].classes = np.load(str('classes'+i+'.npy'), allow_pickle=True)
+		transform = mapper[i].fit_transform(new_df[i])
+		new_df.loc[:,i+'_labels'] = pd.Series(transform, index=new_df.index)
+	labeled = new_df[['yearOfReg','powerPS','kilometer','monthOfRegistration'] + [x+'_labels' for x in labels]]
+
+	X = labeled.values.tolist()
+	print('\n\n', X)
+	predict = reg_model.predict(X)
+
+
+	#predict = predictions['predictions'][0]['values'][0][0]
+	print("Final prediction :",predict)
+
+	return render_template('predict.html',predict=predict)
+
+if __name__=='__main__':
+	reg_model = load_model()#load the saved model
+	app.run(debug=True)
